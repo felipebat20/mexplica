7166148b655ea584d6e24f5a5e28448decaa7779
felipebatista24680@gmail.com <felipebatista24680@gmail.com>
2020-04-13 14:32:16 -0300

    Image upload example implemented

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
+routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
+    console.log(req.file);
     return res.json({
         message: "Tudo certo?"
     });
