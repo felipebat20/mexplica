d38da7409b05331e0702f6f5fa395d2914d97f7b
felipebatista24680@gmail.com <felipebatista24680@gmail.com>
2020-04-20 08:52:07 -0300

    monitoria controller

--- src/controllers/MonitoriaController.js
+++ src/controllers/MonitoriaController.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     async index(req, res) {
         const user_id = req.params.user_id;
-        const monitorias = await Monitoria.findAll({ where: { user_id: user_id } });
+        const monitorias = await Monitoria.findAll();
 
         return res.json(monitorias);
     },
@@ -31,9 +31,39 @@ module.exports = {
         return res.json(monitoria);
     },
     async update(req, res) {
+        const { user_id, id } = req.params;
+        const user = await User.findByPk(user_id);
+        if (!user) {
+            return res.status(400).json({ erro: "User not found" });
+        }
+        const monitoria = await Monitoria.findByPk(id);
+        if (!monitoria) {
+            return res.status(400).json({ erro: "Monitoria not found" });
+        }
 
+        const { title, scope, description } = req.body;
+        try {
+            const result = await Monitoria.update({ title, scope, description }, { where: { user_id, id } });
+            const monitoria = await Monitoria.findByPk(id);
+            return res.json(monitoria);
+        } catch (err) {
+            return res.status(200).json({ erro: err });
+        }
     },
     async delete(req, res) {
-
+        const { user_id, id } = req.params;
+        const user = await User.findByPk(user_id);
+        if (!user) {
+            return res.status(400).json({ erro: "User not found" });
+        }
+        const monitoria = await Monitoria.findByPk(id);
+        if (!monitoria) {
+            return res.status(400).json({ erro: "Monitoria not found" });
+        }
+        if (user_id != monitoria.user_id) {
+            return res.status(400).json({ erro: 'O usuário e monitoria não coincidem' });
+        }
+        await Monitoria.destroy({ where: { id } });
+        return res.status(200).json({ message: 'Monitoria deletada com sucesso!' });
     },
 }
